490c153d0249410b3e171f4519ca8f0cff3a7f47
Aki-7 <aki.develop8128@gmail.com>
2020-04-22 04:08:35 +0900

    refine

--- imap.py
+++ imap.py
@@ -6,37 +6,29 @@ from email import utils
 def info(str):
     print("Info   : ", str)
 
-class SMTPClient:
+class IMAPSClient:
     BUFFER_SIZE = 1000
 
-    def __init__(self, hostname, port = 587):
+    def __init__(self, hostname, port = 993):
         self.hostname = hostname
         self.ipaddr = Util.ns_lookup(hostname)
         self.port = port
         self.socket = None
-        self._sock = None
-        self._ssl_sock = None
         self._ssl_context = ssl.create_default_context()
         self._ssl_context.check_hostname = False
         self._ssl_context.verify_mode = ssl.CERT_NONE
         self.__print_status()
 
     def connect(self):
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._sock.connect((self.ipaddr, self.port))
-        self.socket = self._sock
-        self.receive()
-
-    def start_tls(self):
-        self.send("STARTTLS\n")
-        self._ssl_sock = self._ssl_context.wrap_socket(
-            self._sock,
+        self.socket = self._ssl_context.wrap_socket(
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM),
             server_hostname=self.hostname
         )
-        self.socket = self._ssl_sock
+        self.socket.connect((self.ipaddr, self.port))
+        self.receive()
 
     def send(self, ascii_str, no_print = False, secret = False):
-        self.socket.send((ascii_str).encode("ascii"))
+        self.socket.send((ascii_str).encode())
         if not no_print:
             self.__print_send_message(ascii_str, secret = secret)
         self.receive()
@@ -45,31 +37,39 @@ class SMTPClient:
         while(True):
             try:
                 val = input("[+] Client : ")
-                self.send(val + "\r\n", no_print=True)
+                if val == "":
+                    self.receive()
+                else:
+                    self.send(val + "\r\n", no_print=True)
             except KeyboardInterrupt:
                 break
 
     def close(self):
-        if isinstance(self._ssl_sock, ssl.SSLSocket):
-            self._ssl_sock.close()
-            info("close ssl socket")
-        if isinstance(self._sock, socket.socket):
-            self._sock.close()
+        if isinstance(self.socket, ssl.SSLSocket):
+            self.socket.close()
             info("close socket")
 
     def receive(self, timeout = 0.1):
-        rrdy, wrdy, xrdy = select.select([self.socket], [], [], timeout)
-        for s in rrdy:
-            if not s == self.socket:
+        while(True):
+            rrdy, wrdy, xrdy = select.select([self.socket], [], [], timeout)
+            if len(rrdy) == 0:
                 break
-            lines = s.recv(self.BUFFER_SIZE)
-            for line in lines.splitlines():
-                self.__print_response(line)
 
-    def __print_response(self, bin_s):
-        string = bin_s.decode("ascii")
+            for s in rrdy:
+                if not s == self.socket:
+                    break
+                res = s.recv(self.BUFFER_SIZE)
+                multiline = len(res.splitlines()) > 1
+                self.__print_response(res, multiline=multiline)
+
+    def __print_response(self, bin_s, multiline = False):
+        string = bin_s.decode()
         end = "" if string.endswith("\n") else "\n"
-        print("[-] Server : ", string , end=end)
+        if multiline:
+            print("[-] Server :")
+            print(string, end=end)
+        else:
+            print("[-] Server :", string , end=end)
 
     def __print_send_message(self, s, secret = False):
         if secret:
@@ -95,15 +95,6 @@ class Util:
         return addrs[0][4][0]
 
     @classmethod
-    def b64encoded_account(cls, username, password):
-        str = "{name}\0{name}\0{passwd}".format(name=username, passwd=password)
-        return base64.b64encode(str.encode()).decode() + "\r\n"
-
-    @classmethod
-    def b64decode(cls, string):
-        return base64.b64decode(string).decode()
-
-    @classmethod
     def now(cls):
         now = datetime.datetime.now()
         nowtuple = now.timetuple()
@@ -111,25 +102,27 @@ class Util:
         rfc822 = utils.formatdate(nowtimestamp)
         return rfc822
 
+    @classmethod
+    def b64decode(cls, string):
+        return base64.b64decode(string).decode()
+
 if __name__ == "__main__":
     username = os.environ["MAIL_USERNAME"]
     password = os.environ["MAIL_PASSWORD"]
     hostname = "<...>"
-    client = SMTPClient(hostname, 587)
+    client = IMAPSClient(hostname)
     client.connect()
-    client.start_tls()
-    client.send("AUTH PLAIN\r\n")
-    auth = Util.b64encoded_account(username=username, password=password)
-    client.send(auth)
-    client.send("MAIL FROM:<sample@sample.com>\r\n")
-    client.send("RCPT TO:<sample@sample.com>\r\n")
-    client.send("DATA\r\n")
-    client.send("FROM: Aki\r\n")
-    client.send("TO: Ika\r\n")
-    client.send("Date: {}\r\n".format(Util.now()))
-    client.send("Subject: \(- - )")
-    client.send("\r\n")
-    client.send("Hello SMTP!\r\n")
-    client.send(".\r\n")
+    auth = ". login {} {}\r\n".format(username, password)
+    client.send(auth, secret=True)
+    client.send('. list "" "%"\r\n')
+    client.send('. select inbox\r\n')
+    client.send('. fetch 12 bodystructure\r\n')
+    client.send('. fetch 12 envelope\r\n')
+    client.send('. search (text "aki")\r\n')
+    time.sleep(3)
     client.receive()
-    client.send("QUIT\r\n")
+    client.send('. fetch 12 body[0]\r\n')
+    client.send('. fetch 12 body[1]\r\n')
+    client.send('. fetch 12 body[1]\r\n')
+    client.send('. fetch 261 body[1]\r\n')
+    client.send('. fetch 261 body[1]\r\n')
